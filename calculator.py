9cd1a1a5e879cd0d89e26a5d584f925198769a2c
Sveta <sveta.zheleznova.03@mail.ru>
2021-11-22 19:38:59 +0300

    numbers

--- calculator.py
+++ calculator.py
@@ -28,6 +28,27 @@ class Calculator(QWidget):
         self.b_3 = QPushButton("3", self)
         self.hbox_first.addWidget(self.b_3)
 
+        self.b_4 = QPushButton("4", self)
+        self.hbox_first.addWidget(self.b_4)
+
+        self.b_5 = QPushButton("5", self)
+        self.hbox_first.addWidget(self.b_5)
+
+        self.b_6 = QPushButton("6", self)
+        self.hbox_first.addWidget(self.b_6)
+
+        self.b_7 = QPushButton("7", self)
+        self.hbox_first.addWidget(self.b_7)
+
+        self.b_8 = QPushButton("8", self)
+        self.hbox_first.addWidget(self.b_8)
+
+        self.b_9 = QPushButton("9", self)
+        self.hbox_first.addWidget(self.b_9)
+
+        self.b_0 = QPushButton("0", self)
+        self.hbox_first.addWidget(self.b_0)
+
         self.b_dot = QPushButton(".", self)
         self.hbox_first.addWidget(self.b_dot)
 
@@ -55,6 +76,13 @@ class Calculator(QWidget):
         self.b_1.clicked.connect(lambda: self._button("1"))
         self.b_2.clicked.connect(lambda: self._button("2"))
         self.b_3.clicked.connect(lambda: self._button("3"))
+        self.b_4.clicked.connect(lambda: self._button("4"))
+        self.b_5.clicked.connect(lambda: self._button("5"))
+        self.b_6.clicked.connect(lambda: self._button("6"))
+        self.b_7.clicked.connect(lambda: self._button("7"))
+        self.b_8.clicked.connect(lambda: self._button("8"))
+        self.b_9.clicked.connect(lambda: self._button("9"))
+        self.b_0.clicked.connect(lambda: self._button("0"))
         self.b_dot.clicked.connect(lambda: self._button("."))
 
 
